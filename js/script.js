40ef4ec647a10f7f0b1d28deba1c2a7d3d6c202a
Jeff <jeff.elkind11@gmail.com>
2016-12-02 11:31:04 -0800

    such update, many amaze, much bug

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
     document.getElementById("main").style.marginLeft = "200px";
-    document.getElementById("jumbotron").style.marginLeft = "350px";
+    document.getElementById("jumbotron").style.marginLeft = "250px";
     $('#toggle-div').hide();
 }
 
@@ -14,4 +14,12 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.getElementById("jumbotron").style.marginLeft = "100px";
     $('#toggle-div').show();
+}
+
+function openLogin() {
+    $('.overlay, .login-form').show();
+}
+
+function closeLogin() {
+    $('.overlay, .login-form').hide();
 }
